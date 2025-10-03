60ee456c397276e8532803463f1eb828f9162488
Maria Mello <malumelloms@gmail.com>
2025-10-03 15:55:03 -0300

    a

--- 4_variaveis/7_escopo_global/index.php
+++ 4_variaveis/7_escopo_global/index.php
@@ -9,3 +9,22 @@
 
         echo "$teste global 1 <br>";
     }
+
+    echo "$teste global 2<br>";
+
+    function funcao ( ){
+
+        $teste = "Aqui aqui String";
+        echo "$teste global 2<br>";
+
+    }
+
+    funcao ( );
+
+    function testandoGlobal ( ){
+
+        $teste = "Cossorro";
+        echo "$teste global função<br>";
+
+    }
+    testandoGlobal( );
