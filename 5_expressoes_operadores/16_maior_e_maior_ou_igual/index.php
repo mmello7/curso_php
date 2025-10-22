b4233660fbfaaa3c42ab0fa7337518fc878a97d4
Maria Mello <malumelloms@gmail.com>
2025-10-22 13:51:12 -0300

    Maior e maior ou igual

---
+++ 5_expressoes_operadores/16_maior_e_maior_ou_igual/index.php
@@ -0,0 +1,28 @@
+<?php
+
+        $a = 4;
+        $b = 3;
+        $c = 8;
+        $d = 4;
+        
+        echo "A: ". $a . "<br>";
+        echo "B: ". $b . "<br>";
+        echo "C: ". $c . "<br>";
+        echo "D: ". $d . "<br>";
+        echo "<br>";
+
+        if ($a >$b){
+            echo "A é maior que B <br>";
+        }
+
+        if ($b > $a){
+            echo "B é maior que A <br>";
+        }
+
+        if ($a > $c){
+            echo "A é maior que C <br>";
+        }
+
+        if ($c >= $a){
+            echo "C é maior ou igual a A <br>";
+        }
