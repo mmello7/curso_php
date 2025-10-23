426237272dae3590892939876e483607e3f374ec
Maria Mello <malumelloms@gmail.com>
2025-10-23 16:08:17 -0300

    Me perdi

---
+++ 5_expressoes_operadores/21_cast/index.php
@@ -0,0 +1,27 @@
+<?php
+
+    $a = (int) "12" ;
+
+    echo $a;
+    echo "<br>";
+    echo $a + 10;
+    echo "<br>";
+
+    if ($a === 12){
+        echo "A é identico a 12";
+    }
+
+    $b = (float) "3.14843824";
+
+    if ($b === 3.14843824){
+        echo "B é identico a 3.14843824 <br>";
+    }
+
+    $c = (string) 34;
+
+    echo $c;
+    echo "<br>";
+
+    if ($c === "34"){
+        echo "C é identico à 34";
+    }
