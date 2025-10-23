30084815b05ebb4e499211a7922bef1a6e5411b2
Maria Mello <malumelloms@gmail.com>
2025-10-23 15:10:25 -0300

    And, or, Not

--- 5_expressoes_operadores/18_operadores_logicos/and.php
+++ 5_expressoes_operadores/18_operadores_logicos/and.php
@@ -1,3 +1,18 @@
 <?php
 
-    
+    if (5>10 && 10>5){ //false e true
+        echo "Entrou no if 1 <br>";
+    }
+
+    if (5>2 && 50>10){ //true and true
+        echo "Entrou no if 2 <br>";
+    }
+
+    $a = 12;
+    $b = 10;
+    $c = 4;
+    $d = 4;
+
+    if ($a > $b && $c == $d){
+        echo "Entrou no if 3<br>";
+    }
