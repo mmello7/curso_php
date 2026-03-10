487b76694816ebf5c6aea6a46d53a9466cb76e8d
Maria Mello <malumelloms@gmail.com>
2026-03-10 16:36:45 -0300

    Else

---
+++ 6_estruturas_controle/4_else/index.php
@@ -0,0 +1,13 @@
+<?php
+
+if (5 > 2) {
+    echo "Entrou no if <br>";
+} else {
+    echo "Entrou no else <br>";
+}
+
+if (5 > 22) {
+    echo "Entrou no if <br>";
+} else {
+    echo "Entrou no else <br>";
+}
